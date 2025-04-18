adb0ddac9ebecbbd745cc237759b2d7466518a77
heymachineni <your-email@example.com>
2025-04-18 12:58:25 +0530

    Fix Next.js deployment: Add proper Next.js pages structure and update configuration

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../src/index.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp; 
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import App from '../src/App';
+import '../src/index.css';
+
+export default function Home() {
+  return <App />;
+} 
